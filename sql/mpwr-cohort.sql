6966aaabfc359b045b20bb71b589935ae299cc55
Alistair Johnson <alistairewj@gmail.com>
2017-05-19 14:59:29 -0400

    add example exclusion summary queries

--- sql/mpwr-cohort.sql
+++ sql/mpwr-cohort.sql
@@ -58,3 +58,13 @@ left join public.ventdurations v
 left join mpwr_trach tr
   on t1.icustay_id = tr.icustay_id
 order by t1.icustay_id;
+
+-- Summarize exclusions
+-- select
+-- count(*) as total_num
+-- , sum(exclusion_nonadult) as exclusion_nonadult
+-- , sum(exclusion_readmission) as exclusion_readmission
+-- , sum(exclusion_trach) as exclusion_trach
+-- , sum(exclusion_not_vent) as exclusion_not_vent
+-- , sum(exclusion_bad_data) as exclusion_bad_data
+-- from mpwr_cohort;
