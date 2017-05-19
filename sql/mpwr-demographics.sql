4242fe68bf9f6ec7223eb774df314ea45733019a
Alistair Johnson <alistairewj@gmail.com>
2017-05-19 10:53:49 -0400

    add outcomes, total vent dur, fix syntax bugs

--- sql/mpwr-demographics.sql
+++ sql/mpwr-demographics.sql
@@ -1,4 +1,6 @@
-with serv
+DROP TABLE IF EXISTS mpwr_demographics CASCADE;
+CREATE TABLE mpwr_demographics as
+with serv as
 (
   select ie.icustay_id
     , se.curr_service as first_service
@@ -7,7 +9,6 @@ with serv
   left join services se
     on ie.hadm_id = se.hadm_id
     and se.transfertime < ie.intime + interval '6' hour
-
 )
 , terms as (
   SELECT subject_id
@@ -28,8 +29,9 @@ with serv
       else 0
   end as smokingterm
   FROM  noteevents as ne
-),
-smoking_merged as (
+)
+, smoking_merged as
+(
   SELECT subject_id,
   	 CASE                        --  Define smoking variable:
   	 WHEN nonsmoking  = 1 THEN 0 --  0 means patient doesn't smoke -> when there is a negative terms present
@@ -57,6 +59,13 @@ smoking_merged as (
     on di.icd9_code = dicd.icd9_code
   where di.seq_num = 1
 )
+, totalventdur as
+(
+  select icustay_id
+    , sum(extract(epoch from endtime - starttime))/60.0/60.0/24.0 as ventduration_days
+  from ventdurations
+  group by icustay_id
+)
 select co.icustay_id
   -- patient characteristics
   , hw.height_first as height
@@ -70,15 +79,31 @@ select co.icustay_id
   -- admission characteristics
   , a.admission_type
   , a.admission_location as source_of_admission
-  , a.insurance, a.marital_status, a.ethnicity
-  , a.hospital_expire_flag
+  , a.insurance
+  , a.marital_status
+  , a.ethnicity
   , se.first_service
+
+  -- outcomes
+  , a.hospital_expire_flag
+  , case
+      when pt.dod <= a.admittime + interval '30' day then 1
+    else 0 end as thirtyday_expire_flag
+  , case
+      when pt.dod <= a.admittime + interval '1' year then 1
+    else 0 end as oneyear_expire_flag
+  , tvd.ventduration_days
+
+  , extract(epoch from ie.outtime - ie.intime)/60.0/60.0/24.0 as icu_los_days
+  , extract(epoch from a.dischtime - a.admittime)/60.0/60.0/24.0 as hosp_los_days
 
   , primary_dx.icd9_code
   , primary_dx.short_title
   , primary_dx.long_title
 
 from mpwr_cohort co
+inner join icustays ie
+  on co.icustay_id = ie.icustay_id
 inner join admissions a
   on co.hadm_id = a.hadm_id
 inner join patients pt
@@ -92,4 +117,6 @@ left join primary_dx
   and se.rn = 1
 left join smk
   on co.subject_id = smk.subject_id
-order by hadm_id
+left join totalventdur tvd
+  on co.icustay_id = tvd.icustay_id
+order by co.icustay_id;
