3174139f939222c2dcf9773ebf6e06a665d6a735
Alistair Johnson <alistairewj@gmail.com>
2017-05-19 11:03:32 -0400

    fix buggy joins, moved smoker to other table

--- sql/mpwr-demographics.sql
+++ sql/mpwr-demographics.sql
@@ -10,45 +10,6 @@ with serv as
     on ie.hadm_id = se.hadm_id
     and se.transfertime < ie.intime + interval '6' hour
 )
-, terms as (
-  SELECT subject_id
-  ,CASE
-      --               catches negative terms as 'never smoked', 'non-smoker', 'Pt is not a smoker'
-      WHEN ne.text ~* '(never|not|not a|none|non|no|no history of|no h\/o of|denies|denies any|negative)[\s-]?(smoke|smoking|tabacco|tobacco|cigar|cigs)'
-      --               catches negative terms as: 'Tobacco: denies.', 'Smoking: no;'
-      OR   ne.text ~* '(smoke|smoking|tabacco|tobacco|tabacco abuse|tobacco abuse|cigs|cigarettes):[\s]?(no|never|denies|negative)'
-      --               catches negative terms: 'Cigarettes: Smoked no [x]', 'No EtOH, tobacco', 'He does not drink or smoke'
-      OR   ne.text ~* 'smoked no \[x\]|no etoh, tobacco|not drink alcohol or smoke|not drink or smoke|absence of current tobacco use|absence of tobacco use'
-      then 1
-      else 0
-  end as nonsmoking
-  ,CASE
-      --               catches all terms related to smoking: 'Pt. with long history of smoking', 'Smokes 10 cigs/day', 'nicotine patch'
-      WHEN ne.text ~* '(smoke|smoking|tabacco|tobacco|cigar|cigs|marijuana|nicotine)'
-      then 1
-      else 0
-  end as smokingterm
-  FROM  noteevents as ne
-)
-, smoking_merged as
-(
-  SELECT subject_id,
-  	 CASE                        --  Define smoking variable:
-  	 WHEN nonsmoking  = 1 THEN 0 --  0 means patient doesn't smoke -> when there is a negative terms present
-           WHEN smokingterm = 1 THEN 1 --  1 means patient smokes        -> when there is a positive term present and no negative term
-           ELSE 2 END AS smoking       --  2 means unknown               -> no negatieve or smoking terms mentioned at all
-  FROM terms
-)
--- From multiple notes the query takes the min, so if it's mention the patient is a smoker (1)
--- and that the patient is a non-smoker (0), the function decides that the patient is a non-smoker (0)
--- because the negative terms are more explicit and probably not triggered by accident.
-, smk as
-(
-  SELECT subject_id,min(smoking) as smoking
-  from smoking_merged
-  group by subject_id
-  order by subject_id
-)
 -- primary icd-9 code
 , primary_dx as
 (
@@ -107,15 +68,15 @@ inner join icustays ie
 inner join admissions a
   on co.hadm_id = a.hadm_id
 inner join patients pt
-  on co.subject_id = a.subject_id
+  on co.subject_id = pt.subject_id
 left join heightweight hw
   on co.icustay_id = hw.icustay_id
 left join serv se
   on co.icustay_id = se.icustay_id
+  and se.rn = 1
 left join primary_dx
   on co.hadm_id = primary_dx.hadm_id
-  and se.rn = 1
-left join smk
+left join mpwr_smoker smk
   on co.subject_id = smk.subject_id
 left join totalventdur tvd
   on co.icustay_id = tvd.icustay_id
