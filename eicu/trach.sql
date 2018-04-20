0209c47365874d435b2c6f71a8fbdf36b00ade54
Alistair Johnson <alistairewj@gmail.com>
2018-04-20 06:05:47 -0400

    remove dummy query

--- eicu/trach.sql
+++
@@ -1,2 +0,0 @@
-pulmonary|surgery / incision and drainage of thorax|tracheostomy|performed during current admission for ventilatory support
-pulmonary|ventilation and oxygenation|tracheal suctioning
